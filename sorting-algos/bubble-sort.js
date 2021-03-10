d59afb04f796c52f2aa96e541a3c6025f52805bd
Matteo <lingio94@gmail.com>
2021-03-10 12:43:20 +0100

    fix

--- sorting-algos/bubble-sort.js
+++ sorting-algos/bubble-sort.js
@@ -2,14 +2,13 @@ const swap = (arr, indI, indJ) => {
   [arr[indI], arr[indJ]] = [arr[indJ], arr[indI]];
 };
 const bubble = arr => {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] > arr[j]) {
-        swap(arr, i, j);
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
       }
     }
   }
   return arr;
 };
-
-bubble([2, 1, 7, 4, 5]);
+console.log(bubble([2, 1, 7, 4, 5]));
