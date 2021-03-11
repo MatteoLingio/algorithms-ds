7c15e784d30e2773f3944e08bb701418cda95c04
Matteo <lingio94@gmail.com>
2021-03-11 17:21:47 +0100

    insertion sort second impl

--- sorting-algos/insertion-sort.js
+++ sorting-algos/insertion-sort.js
@@ -1,5 +1,17 @@
 //Loop over every element in the array
 //check if is greater than first
+function insertionSort(arr) {
+  var currentVal;
+  for (var i = 1; i < arr.length; i++) {
+    currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentVal;
+  }
+  return arr;
+}
+
 const insertionSort = arr => {
   for (let i = 1; i < arr.length; i++) {
     for (let j = 0; j != i; j++) {
